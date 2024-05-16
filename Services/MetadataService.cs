818255d0f0f77de1a09422e46f8c5c3b99bb91aa
Aleks LovesTech <alekslovestech@gmail.com>
2024-05-16 05:45:15 +0200

    pseudocode for getting assets by id

--- Services/MetadataService.cs
+++ Services/MetadataService.cs
@@ -9,5 +9,14 @@ public interface IMetadataService
 
 public class MetadataService : IMetadataService
 {
-    // Implement methods to read from database and link metadata
+    Task<Asset> GetAssetByIdAsync(string assetId)
+    {
+        //Read the JSON file into an asset array => Deserialize the JSON content.
+        // OR 
+        // Query the database to get the asset by its ID
+        // OR
+        // Query the CMS to get the asset by its ID
+
+        // Find and return the asset by its ID
+    }
 }
